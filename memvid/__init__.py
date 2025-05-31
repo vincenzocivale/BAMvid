43b568c039209d808f4d58d2f46a519eb0ad7d99
Tyler "Nex" Kolody <TJKolody@gmail.com>
2025-05-30 17:26:56 -0700

    Updated the init to work with the LLM client

--- memvid/__init__.py
+++ memvid/__init__.py
@@ -8,5 +8,6 @@ from .encoder import MemvidEncoder
 from .retriever import MemvidRetriever
 from .chat import MemvidChat
 from .interactive import chat_with_memory, quick_chat
+from .llm_client import LLMClient, create_llm_client
 
-__all__ = ["MemvidEncoder", "MemvidRetriever", "MemvidChat", "chat_with_memory", "quick_chat"]
+__all__ = ["MemvidEncoder", "MemvidRetriever", "MemvidChat", "chat_with_memory", "quick_chat", "LLMClient", "create_llm_client"]
